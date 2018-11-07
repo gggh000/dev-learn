a1220ac26f29990c7abef47a3674c081c4c3c10a
Guyen Gn 10.0.0.160 <guyen000@gmail.com>
2018-11-07 00:21:54 -0800

    linkedlist.c: implemented printNode and printLinkedList

--- c/linkedlist.c
+++ c/linkedlist.c
@@ -1,5 +1,46 @@
+#include <stdio.h>
+
+typedef struct sNode 
+{
+    int value;
+    struct sNode * ptrNext;
+} node; 
+
+void printNode(node * pNode) {
+    if (pNode == NULL) {
+        printf("Error. Pointer is null.");
+        return ;
+    }
+
+    printf("%x: %d.\n", pNode->ptrNext, pNode->value);
+    return;
+}
+
+void printLinkedList(node * pNode) {
+    node * lNode = pNode;
+
+    while (pNode != NULL) {
+        printNode(lNode);
+
+        if (lNode->ptrNext != NULL) {
+            lNode = lNode->ptrNext;                
+        } else {
+            return; 
+        }
+    }
+}
+
 int main()
 {
+    node * n1;
+    n1 = malloc(sizeof(node));
+
+    n1->value = 100;
+    n1->ptrNext = NULL;
+
+    //printNode(n1);        
+    printLinkedList(n1);
     printf("done.\n");
     return 0;
 }
+
