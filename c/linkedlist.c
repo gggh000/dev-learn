c7da7e7e3049b272c42fc93c741149fb3a3045fc
Guyen Gn 10.0.0.160 <guyen000@gmail.com>
2018-11-07 00:59:54 -0800

    linkedlist.c: insertNodeFront got it working

--- c/linkedlist.c
+++ c/linkedlist.c
@@ -14,7 +14,7 @@ void printNode(node * pNode) {
         return ;
     }
 
-    printf("%x: %d.\n", pNode->ptrNext, pNode->value);
+    printf("%08x: %04d.\n", pNode, pNode->value);
     return;
 }
 
@@ -32,8 +32,30 @@ void printLinkedList(node * pNode) {
     }
 }
 
-void insertNodeFront(node * ptrNode) {
-    return; 
+//  Inserts node at the front of linked list.
+//  input:
+//  - node * ptrNode - pointer to the node pointer. Note that since head pointer of the list is changin
+//  when we are inserting at the front, therefore pointer to head pointer must be passed.
+//  - int value - node value to be inserted as new node.
+
+int insertNodeFront(ptrNode * pNode, int pValue) {
+
+    node * lNode;
+    lNode = malloc(sizeof(node));
+    lNode->value = pValue;
+    lNode->ptrNext = NULL;
+
+    printf("insertNodeFront: created new node: %08x: %04d.\n", lNode, lNode->value);
+
+    if (lNode == NULL) {
+        printf("malloc error:");
+        return 1;
+    }
+
+    lNode->ptrNext = * pNode;
+    * pNode = lNode;
+
+    return 0; 
 }
 
 int main()
@@ -44,6 +66,16 @@ int main()
     n1->value = 100;
     n1->ptrNext = NULL;
 
+    printf("\nBefore insert...\n");
+    printLinkedList(n1);
+
+    insertNodeFront(&n1, 101);
+
+    insertNodeFront(&n1, 150);
+    insertNodeFront(&n1, 222);
+    insertNodeFront(&n1, 455);
+
+    printf("\nAfter insert...\n");
     printLinkedList(n1);
     printf("done.\n");
     return 0;
