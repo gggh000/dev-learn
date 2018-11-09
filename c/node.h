88cd9d47959c49d4e898a7a08df5f4d166a7d0aa
Guyen Gn 10.0.0.160 <guyen000@gmail.com>
2018-11-08 22:07:00 -0800

    tree implementation

--- c/node.h
+++ c/node.h
@@ -4,7 +4,15 @@
 typedef struct sNode
 {
     int value;
+
+    // following member used only for linkedlist.
+
     struct sNode * ptrNext;
+
+    // following members used only for tree.
+
+    struct sNode * ptrLeft;
+    struct sNode * ptrRight;
 } node;
 
 typedef node * ptrNode;
