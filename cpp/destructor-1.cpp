9ac14730ffc3afb7e875e09c94c3e2f34b57c448
Guyen Gn 10.0.0.160 <guyen000@gmail.com>
2018-11-12 19:30:04 -0800

    destructor-1.cpp: added

---
+++ cpp/destructor-1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class myString
+{
+private:
+
+public:
+    int * num;
+    myString(int pNum)
+    {
+        cout << "Constructor called." << endl;
+
+        if(pNum != NULL) {
+            num = new int(50);
+            cout << "num points to: 0x" << hex;
+            cout << (unsigned int*) num << endl;
+        } else {
+            num = new int(100);
+            cout << "num points to: 0x" << hex;
+            cout << (unsigned int*) num << endl;
+        }
+    }    
+
+    myString(myString& copySource)
+    {
+        num = NULL;
+        cout << "Copy constructor: copying from copySource" << endl;
+
+        if (copySource.num != NULL)
+        {
+            num = new int(10);
+            cout << "num points to: 0x" << hex;
+            cout << (unsigned int*) num << endl;
+        }
+    }
+    
+    ~myString()
+    {
+        cout << "Destructor called, deleting num ptr: " << num << ": 0x" << hex <<  (unsigned int*)num << endl;
+        delete num;
+    }
+};
+
+void useMyString(myString str)
+{
+    cout << "entered useMyString()" << endl;
+}
+
+int main()
+{
+    myString str1(24);
+    // useMyString(str1);    
+    myString str2(str1);
+
+    cout << "str1: " << " " << (str1.num) << endl;
+    cout << "str2: " << " " << (str2.num) << endl;
+}
