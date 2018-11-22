3441a813d97181676206c43a06a64abe8664bef9
Guyen Gn 10.0.0.160 <guyen000@gmail.com>
2018-11-22 00:14:01 -0800

    ptr-ref.cpp: enhancement.

--- cpp/pointer-ref-game/ptr-ref.cpp
+++ cpp/pointer-ref-game/ptr-ref.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int f1_ref(int & pInt)
+{
+    cout << "f1_ref: &pInt/pInt:   :" << &pInt << ", " << pInt << endl;
+    return 0;    
+}
+
+int f1_ptr(int * pInt)
+{
+    cout << "f1_ref: pInt/*pInt:   :" << pInt << ", " << *pInt << endl;
+    return 0;    
+}
+
 int f1_copy(int pInt) 
 {
+    cout << "f1_copy: &pInt/pInt:   :" << &pInt << ", " << pInt << endl;
     return 0;    
 }
 
@@ -17,4 +30,11 @@ int main()
     cout << "address of int2        :" << &int2 << endl;
     cout << "address held by intPtr1:" << intPtr1 << endl;
     cout << "address of intRef      :" << &intRef << endl;
+    
+    f1_copy(int1);
+    f1_copy(int2);
+    f1_ref(int1);
+    f1_ref(int2);
+    f1_ptr(&int1);
+    f1_ptr(&int2);
 }
