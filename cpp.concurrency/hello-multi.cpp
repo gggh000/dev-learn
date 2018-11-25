4d72a82de7949cd0eb6b7ccaa6615b82f3fef558
Guyen Gn 10.0.0.160 <guyen000@gmail.com>
2018-11-24 20:37:45 -0800

    multiple threads launched and detachment success.

--- cpp.concurrency/hello-multi.cpp
+++ cpp.concurrency/hello-multi.cpp
@@ -12,11 +12,11 @@ using namespace std;
 
 void hello(int pId, int pStat[])
 {
-    int sleep_duration = rand() % 10 + 20;
+    int sleep_duration = rand() % 5 + 10;
 	std::cout << pId << ": Hello CONCURRENT WORLD, sleeping for " << sleep_duration << endl; 
     sleep(sleep_duration);
     pStat[pId]  = 1;
-    std::cout << pId << ": done sleeping exiting now...";
+    std::cout << pId << ": done sleeping exiting now..." << endl;
 }
 
 int main()
@@ -24,26 +24,25 @@ int main()
     int i;
     const int CONFIG_THREAD_COUNT = 10;
     int stat[CONFIG_THREAD_COUNT];
-    int sum;
+    int sum = 0;
 
     for ( i = 0; i < CONFIG_THREAD_COUNT; i ++ ) {
+        stat[i] = 0;
         std::thread t(hello, i, stat);
         t.detach();
     }
 
-    sleep(50);
     cout << "Checking thread status-s..." << endl;
 
-    /*
      while (sum != 10)  {
         sum = 0;
 
         for (i = 0; i < CONFIG_THREAD_COUNT; i++) {
+            cout << stat[i] << ", ";
             sum += stat[i];
         }
 
-        sleep(5);
         cout << "main(): sum: " << sum << ". waiting for all threads to finish..." << endl;
+        sleep(5);
     }
-    */
 }
