573aefa0329d6ee72eed1c4a625a18e311cc8a06
Guyen Gn 10.0.0.160 <guyen000@gmail.com>
2018-11-28 23:08:17 -0800

    CH3-2-1-mutex.cpp: thread code imported

--- cpp.concurrency/CH3-2-1-mutex.cpp
+++ cpp.concurrency/CH3-2-1-mutex.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+void hello(int pId, int pStat[])
+{
+    int sleep_duration = rand() % 30 + 20;
+    std::cout << "(" << hex << this_thread::get_id() << ")" << pId << ": Hello CONCURRENT WORLD, sleeping for " << sleep_$
+    sleep(sleep_duration);
+    pStat[pId]  = 1;
+    std::cout << pId << ": Done sleeping exiting now..." << endl;
+}
+
 class data_wrapper
 {
 private:
@@ -35,6 +44,30 @@ public:
 
 int main()
 {
+    int i;
+    const int CONFIG_THREAD_COUNT = 10;
+    int stat[CONFIG_THREAD_COUNT];
+    int sum = 0;
+
+    for ( i = 0; i < CONFIG_THREAD_COUNT; i ++ ) {
+        stat[i] = 0;
+        std::thread t(hello, i, stat);
+        t.detach();
+    }
+
+    cout << "Checking thread status-s..." << endl;
+
+     while (sum != CONFIG_THREAD_COUNT)  {
+        sum = 0;
+
+        for (i = 0; i < CONFIG_THREAD_COUNT; i++) {
+            cout << stat[i] << ", ";
+            sum += stat[i];
+        }
+
+        cout << "main(): sum: " << sum << ". waiting for all threads to finish..." << endl;
+        sleep(5);
+    }
 
     return 0;
 }
