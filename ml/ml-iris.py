0904b4843e364df9dbf419fbdac8f4232529b142
guyen <guyen000@gmail.com>
2018-12-17 02:10:10 -0800

    panda-plot.py: added, works

--- ml/ml-iris.py
+++ ml/ml-iris.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from sklearn.datasets import load_iris
 import pandas as pd
-#import mglearn 
+import mglearn 
 iris_dataset = load_iris()
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
@@ -21,5 +21,6 @@ print("y_train shape: {}".format(y_train.shape))
 print("X_test shape: {}".format(X_test.shape))
 print("y_test shape: {}".format(y_test.shape))
 
-#pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15,15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15,15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+#pd.show()
 
---
+++ ml/panda-plot.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+ts = ts.cumsum()
+ts.plot()
+plt.show()
