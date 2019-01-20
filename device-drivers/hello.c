db669eb1e1a844723faf86cf5b7106cb23565ada
Your Name <you@example.com>
2019-01-19 20:22:34 -0500

    c.concurrency files: added

--- device-drivers/hello.c
+++ device-drivers/hello.c
@@ -1,3 +1,4 @@
+// centos 72
 ///usr/src/kernels/3.10.0-327.el7.x86_64/arch/x86/include/asm/init.h
 ///usr/src/kernels/3.10.0-327.el7.x86_64/include/config/debug/memory/init.h
 ///usr/src/kernels/3.10.0-327.el7.x86_64/include/config/provide/ohci1394/dma/init.h
@@ -10,6 +11,7 @@
 ///usr/src/kernels/3.10.0-327.el7.x86_64.debug/include/asm-generic/linkage.h
 ///usr/src/kernels/3.10.0-327.el7.x86_64.debug/include/linux/linkage.h
 
+// centos65
 ///usr/src/kernels/2.6.32-754.10.1.el6.x86_64.debug/arch/x86/include/asm/init.h
 ///usr/src/kernels/2.6.32-754.10.1.el6.x86_64.debug/include/config/debug/memory/init.h
 ///usr/src/kernels/2.6.32-754.10.1.el6.x86_64.debug/include/config/hermes/cache/fw/on/init.h
@@ -20,11 +22,13 @@
 ///home/pxeboot/syslinux/syslinux-6.03/gpxe/src/include/gpxe/init.h
 ///home/pxeboot/syslinux/syslinux-6.03/core/lwip/src/include/lwip/init.h
 
+#ifndef _LINUX_INIT_H
+#define _LINUX_INIT_H
 
 #include <linux/init.h>
 #include <linux/module.h>
 
-MODULE_LICENSE("Dual BSD/GPL")
+#MODULE_LICENSE("Dual BSD/GPL")
 
 static int hello_init(void) {
 //  printk(KERN_ALERT "Hello, world.\n");
@@ -39,3 +43,5 @@ static void hello_exit(void) {
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+#endif
