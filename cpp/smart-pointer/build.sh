712d98a9ab5882b0f522cf2378ac6cd6a0a7c220
Your Name <you@example.com>
2019-01-22 00:44:21 -0500

    shared-ptr.cpp: added, build.sh: improvement

--- cpp/smart-pointer/build.sh
+++ cpp/smart-pointer/build.sh
@@ -1,2 +1,13 @@
-g++ unique-ptr.cpp  -std=c++0x
-./a.out
+UNIQUE_PTR=unique-ptr
+SHARED_PTR=shared-ptr
+
+FILES=( \
+$UNIQUE_PTR \
+$SHARED_PTR \
+)
+
+for (( i = 0 ; i < ${#FILES[@]} ; i ++ ))
+do
+    echo building ${FILES[$i]} ...
+    g++ ${FILES[$i]}.cpp  -std=c++0x -o ${FILES[$i]}.out
+done
---
+++ cpp/smart-pointer/shared-ptr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+#include <utility>
+
+using namespace std;
+ 
+int main() {
+    shared_ptr<int> valuePtr (new int (222) );
+    cout << "valuePtr: " << *valuePtr << endl;
+    shared_ptr<int> valuePtrNow(move(valuePtr));
+    cout << "valuePtrNow: " << *valuePtrNow << endl;
+
+    if (valuePtr != NULL)
+        printf("0x%x", valuePtr);
+    else    
+        printf("valuePtr is null now.")
+
+    if (valuePtrNow != NULL)
+        printf("0x%x", valuePtrNow);
+    else
+        printf("valuePtrNow is null now.")
+    return 0;
+}
+
