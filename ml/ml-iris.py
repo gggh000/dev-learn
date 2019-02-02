3d802df0157df9bd6127619c19490d97811ed245
Guyen Gankhuyag (ggankhuy) <ggankhuy@cisco.com>
2019-02-01 17:20:17 -0800

    prediction accuracy score added

--- ml/ml-iris.py
+++ ml/ml-iris.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+
+def print_k(pStr):
+	print("-------------------------------------------------------------")
+	print(str(pStr))
+	print("-------------------------------------------------------------")
+	
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier 
 
@@ -26,16 +32,25 @@ print("y_train shape: {}".format(y_train.shape))
 print("X_test shape: {}".format(X_test.shape))
 print("y_test shape: {}".format(y_test.shape))
 
+print_k("(OPTIONAL): SHOW PLOT.")
 pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15,15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
 #plt.show()
 
+print_k("CREATE PREDICTION OBJECT BASED ON K-NEIGHTBOUR CLASSIFIER, KNN OBJECT.")
 knn = KNeighborsClassifier(n_neighbors=1) 
 knn.fit(X_train, y_train)
 
+print_k("PREDICTION FOR SINGLE DATA: X_new")
 X_new = np.array([[5, 2.9, 1, 0.2]]) 
 print("X_new.shape: {}".format(X_new.shape))
 prediction = knn.predict(X_new) 
 print("Prediction: {}".format(prediction)) 
 print("Predicted target name: {}".format(       iris_dataset['target_names'][prediction]))
 
+print_k("PREDICTION FOR X_TEST DATA SET")
+y_pred = knn.predict(X_test) 
+print("Test set predictions:\n {}".format(y_pred))
+
+print_k("PREDICTION ACCURACY SCORE:")
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
 
