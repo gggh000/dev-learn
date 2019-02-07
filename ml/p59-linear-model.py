533adc34c6f7f12b932b56c21614238c599b81ef
Guyen Gankhuyag (ggankhuy) <ggankhuy@cisco.com>
2019-02-06 23:44:37 -0800

    completed ridge linear regression

--- ml/p59-linear-model.py
+++ ml/p59-linear-model.py
@@ -1,2 +1,88 @@
+def print_k(pStr):
+	print("-------------------------------------------------------------")
+	print(str(pStr))
+	print("-------------------------------------------------------------")
+
+'''
+print_k("Plotting simple linear regression model with 1-D wave dataset")
+
+import matplotlib.pyplot as plt
 import mglearn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 mglearn.plots.plot_linear_regression_wave()
+
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+lr = LinearRegression().fit(X_train, y_train)
+
+print("lr.coef_: {}".format(lr.coef_))
+print("lr.intercept_: {}".format(lr.intercept_))
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+plt.plot(X, y, 'o')
+plt.show()
+
+'''
+'''
+import matplotlib.pyplot as plt
+import mglearn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+print_k("Using linear regression model on Boston housing data.")
+X, y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+print X.shape
+print y.shape
+
+print X
+print y
+plt.plot(X, y, '.')
+plt.show()
+
+'''
+
+print_k("Applying ridge linear model on Boston housing data.")
+
+import mglearn
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+X, y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+ridge = Ridge().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))
+
+
+ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge10.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge10.score(X_test, y_test)))
+
+ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge01.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge01.score(X_test, y_test)))
+
+lr = LinearRegression().fit(X_train, y_train)
+
+plt.plot(ridge.coef_, 's', label="Ridge alpha=1")
+plt.plot(ridge10.coef_, '^', label="Ridge alpha=10")
+plt.plot(ridge01.coef_, 'v', label="Ridge alpha=0.1")
+plt.plot(lr.coef_, 'o', label="LinearRegression")
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.hlines(0, 0, len(lr.coef_))
+plt.ylim(-25, 25)
+plt.legend()
+plt.show()
