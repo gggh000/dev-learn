b90a19e5cbb5caf242ca1505523b8bc90b70375e
Gankhuyag <ggankhuy@amd.com>
2019-04-15 12:33:05 -0700

    adding optional + short version

--- python/argparse-ex.py
+++ python/argparse-ex.py
@@ -1,5 +1,20 @@
+'''
+print('Mandatory argument accepting integers')
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", help="display a square of a given number", type=int)
 args = parser.parse_args()
 print(args.square**2)
+print('---------------')
+
+'''
+print('Optional argument that is switched on or off')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbose", "-v", help="increase output verbosity", action="store_true")
+args = parser.parse_args()
+
+if args.verbose:
+    print("verbosity turned on")
+else:
+	print("verbosity off")
