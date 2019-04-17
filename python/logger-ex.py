8c50a87b522f0a4cc8699514e7991d6a0659129f
Your Name <you@example.com>
2019-04-17 13:52:27 -0700

    logging test script, added

---
+++ python/logger-ex.py
@@ -0,0 +1,24 @@
+import logging
+
+logger=logging.getLogger('test-app')
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('test-app-debug.log')
+ch = logging.StreamHandler()
+
+fh.setLevel(logging.DEBUG)
+ch.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+logger.info("test-app-log emitted.")
+
+
+
+
