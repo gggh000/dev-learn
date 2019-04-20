fbe8fe4ec3d7eef49da977c1709fc0ec132139e1
Your Name <you@example.com>
2019-04-20 03:18:53 -0400

    python3-print-conver.py: test ok with print statement replacement

--- python/python3-converter/python3-print-convert.py
+++ python/python3-converter/python3-print-convert.py
@@ -1,23 +1,50 @@
+#!/usr/bin/python3
+
 import os
 import sys
+import re
 
-if not sys.argv[1]:
-	print("You need to input file name: python3 ", sys.argv[0], " <filename>")
-	return 0;
+fp = None
+fp1 = None 
+buffer = None
 
-fp = open(sys.argv[1])
+try:
+	if not sys.argv[1]:
+		print("You need to input file name: python3 ", sys.argv[0], " <filename>")
+		sys.exit(1)
+	
+	if not re.search("\.py$", sys.argv[1]):
+		print("Does not end with .py, Please input python file.")	
+		sys.exit(1)
+	
+	fp = open(sys.argv[1])
+	if not fp:
+		print("Failed to open ", sys.argv[1])
+		sys.exit(1)
 
-if not fp:
-	print("Failed to open ", sys.argv[1])
-	return 0;
+	outputFile = sys.argv[1][:-3] + "3.py".strip()
+	print("output: ", outputFile)
+	fp1 = open(outputFile, 'w')
+	
+	buffer = 1
+	
+	while buffer:
+		buffer = fp.readline()
+		#print("read: ", buffer)
 
-buffer = 1
+		if re.search("print ", buffer):
+			line = re.sub("print ", "print(", buffer).strip()
+			line = line + ")"
+			print(line)	
+			fp1.write(line)	
+		else:
+			fp1.write(buffer)
 
-while buffer:
-	buffer = print file.readline()
-	print("read: ", buffer)
-
-
+except Exception as msg:
+	print(msg)
  
+#fp.close()
+#fp1.close()
+
 	
 
