37966528d10f7f9b2d856d3bf7db1b10ad99079b
Your Name <you@example.com>
2019-08-31 23:20:24 -0700

    vulkantutorial build info added to Makefile, build errors.

--- vulkan/vulkantest/vulkanTutorial-p41.cpp
+++ vulkan/vulkantest/vulkanTutorial-p41.cpp
@@ -9,7 +9,6 @@
 
 void HelloTriangleApplication::initWindow() {
 	glfwInit();
-
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr,
