298d4bad181846001f730ef52b73ad06fbf12aaa
Your Name <you@example.com>
2019-09-03 00:39:49 -0700

    ml-skigit-with-tflow-p44.py: not used two.py

--- ml-ubuntu/tensorflow/ml-skigit-with-tflow-p44.py
+++ ml-ubuntu/tensorflow/ml-skigit-with-tflow-p44.py
@@ -1,12 +1,16 @@
 import os
 import tarfile
 import pandas as pd
-from two import *
+#from two import *
 
 from six.moves import urllib
 DOWNLOAD_ROOT="https://raw.githubusercontent.com/ageron/handson-ml/master"
 HOUSING_PATH=os.path.join("datasets", "housing")
 HOUSING_URL=DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
 
 '''
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
@@ -21,4 +25,6 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 def load_housing_data(housing
 '''
 housing = load_housing_data()
-housing.head()
+print(housing.head())
+housing.info()
+print(housing["ocean_proximity"].value_counts())
