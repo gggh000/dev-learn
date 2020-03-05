48038e950386b9680dda25139f712ba7398c3dce
Your Name <you@example.com>
2020-03-05 15:44:08 -0800

    checkpoint, retrieve issues not working, browser display rest api

--- jira-python/jira-python-localconnect.py
+++ jira-python/jira-python-localconnect.py
@@ -10,13 +10,23 @@ os.system("clear")
 jira_options={'server': "https://ggjira100.atlassian.net/jira}"}
 jira = jira.JIRA('https://ggjira100.atlassian.net', basic_auth=("ggjira000", "61z2Rg0PX8INeZkasl5W75F4"))
 
-#print(jira)
+print(jira)
 
-#print("===================")
-#print("obtaining projects...")
-#projects=jira.projects()
+print("===================")
+print("obtaining projects...")
+projects=jira.projects()
+keys = sorted([project.key for project in projects])[2:5]
 
-#for proj in projects:
-#	print("proj: ", proj)
+'''
+print(projects)
+print("obtaining keys...")
+print(keys)
+issue=jira.issue("issue-1000-1001")
+print(issue)
+'''
 
+#issue=jira.issue("issue-1000-1001")
+#print(issue)
+
+issues_in_proj = jira.search_issues('project=gg-proj-1001')
 
