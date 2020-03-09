0d9cbecbaa74bcec01d2afcef93c9944dbc1b993
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-03-09 13:22:53 -0400

    jira plugin, not working

--- jira-python/jira-python-localconnect.py
+++ jira-python/jira-python-localconnect.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 debug=1
-proj_qualifier_amd_jira="ontrack-internal.amd.com"
+proj_qualifier_amd_jira="http://ontrack-internal.amd.com"
 proj_qualified_ggjira000="https://ggjira000.atlassian.net"   
 os.system("clear")
 
@@ -25,27 +25,33 @@ except Exception as msg:
     print("No project specified in command line. Using default url: https://ggjira100.atlassian.net")
     print(msg)
 
+oauth_dict = {
+'access_token': "61z2Rg0PX8INeZkasl5W75F4",
+#'access_token_secret': 'bar',
+'consumer_key': 'ggjira100token',
+#'key_cert': key_cert_data
+}
+
+
 jira_options={'server': jira_url + "/jira"}
-jira = jira.JIRA('https://ggjira100.atlassian.net', basic_auth=("ggjira000", "61z2Rg0PX8INeZkasl5W75F4"))
-
-print(jira)
-
-
-print("===================")
-print("obtaining projects...")
 
 if jira_url == proj_qualifier_amd_jira:
     print("AMD jira...")
+    #jira = jira.JIRA(jira_options)
+    jira = jira.JIRA(jira_options, basic_auth=("ggankhuy", "197365aFFF#"))
     project=jira.project('SWDEV')
 elif jira_url == proj_qualified_ggjira000:
+    #non-amd...
+    #jira = jira.JIRA(jira_options', basic_auth=("ggjira000", "61z2Rg0PX8INeZkasl5W75F4"), logging=True)
+    #jira = jira.JIRA('https://ggjira100.atlassian.net', oauth=oauth_dict, logging=True)
     project=jira.project('GP0')
 else:
     projects=jira.projects()
+'''
 print(projects)
 print(len(projects))
 #keys = sorted([project.key for project in projects])[2:5]
 
-'''
 print(projects)
 print("obtaining keys...")
 print(keys)
