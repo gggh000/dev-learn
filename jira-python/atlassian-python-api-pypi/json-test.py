31809ba9c82388cd9a4341464f7f3603eea2d5e1
Your Name <you@example.com>
2020-03-08 20:05:57 -0700

    iterate through issue inner data

--- jira-python/atlassian-python-api-pypi/json-test.py
+++ jira-python/atlassian-python-api-pypi/json-test.py
@@ -2,6 +2,8 @@
 
 from atlassian import Jira
 from pprint import pprint
+import sys
+import os
 
 jira = Jira(
     url='http://10.0.0.100:8080',
@@ -24,3 +26,20 @@ print(issues)
 
 for i in issues:
 	pprint(i)
+
+issue1=issues[1]
+for i in issue1.items():
+	print("=============================")
+	print("type: ", type(i), i)
+	print("-----------------------------")
+	try:
+		print("inner type: ", type(i[1]))
+
+		if type(i[1]) == dict:
+			innerData=i[1]
+			for j in innerData.items():
+				print(j)
+	except Exception as msg:
+		print(msg)
+		print("Idx error")
+
