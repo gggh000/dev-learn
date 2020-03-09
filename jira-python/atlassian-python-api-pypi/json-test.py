065f9e54e27ad15c8d6b42e4c797558bd4741271
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-03-09 13:52:02 -0400

    json-test.py: improved coding

--- jira-python/atlassian-python-api-pypi/json-test.py
+++ jira-python/atlassian-python-api-pypi/json-test.py
@@ -4,12 +4,46 @@ from atlassian import Jira
 from pprint import pprint
 import sys
 import os
+import re
+
+JIRA_SERVER_HOME_CLOUD=0
+JIRA_SERVER_HOME_LOCAL=1
+JIRA_SERVER_AMD_CLOUD=2
+JIRA_SERVER_AMD_LOCAL=3
+JIRA_SERVER_AMD_ONTRACK=4
+
+JIRA_SERVER_IDX_URL=0
+JIRA_SERVER_IDX_USER=1
+JIRA_SERVER_IDX_PASS=2
+JIRA_SERVER_IDX_PROJECT=3
+
+jira_server={\
+    JIRA_SERVER_HOME_CLOUD: ['',''],
+    JIRA_SERVER_HOME_LOCAL: [\
+    'http://10.0.0.100:8080', 'ggjira000', '8981555aaa'\
+    'project = gg-proj-mgmt AND status IN ("To Do", "In Progress") ORDER BY issuekey'],
+
+    JIRA_SERVER_AMD_CLOUD: [\
+    'https://ggjira000.atlassian.net', 'ggjira100','8981555aaa', \
+    'project = gg-proj-000 AND status IN ("To Do", "In Progress"\) ORDER BY issuekey'\
+    ]
+}
 
 jira = Jira(
-    url='http://10.0.0.100:8080',
-    username='ggjira000',
-    password='8981555aaa')
-JQL = 'project = gg-proj-mgmt AND status IN ("To Do", "In Progress") ORDER BY issuekey'
+    url=jira_server[JIRA_SERVER_AMD_CLOUD][JIRA_SERVER_IDX_URL],
+    username=jira_server[JIRA_SERVER_AMD_CLOUD][JIRA_SERVER_IDX_USER],
+    password=jira_server[JIRA_SERVER_AMD_CLOUD][JIRA_SERVER_IDX_PASS])
+
+'''
+ jira = Jira(
+-    url='http://10.0.0.100:8080',
+-    username='ggjira000',
+-    password='8981555aaa')
+'''
+
+JQL = jira_server[JIRA_SERVER_AMD_CLOUD][JIRA_SERVER_IDX_PROJECT]
+#JQL = 'project = gg-proj-mgmt AND status IN ("To Do", "In Progress") ORDER BY issuekey'
+
 data = jira.jql(JQL)
 
 '''
@@ -21,6 +55,10 @@ for i in range(0, len(data)):
 	print("-----------------------------")
 	print(list(data.values())[i])
 '''
+if re.search("Basic auth with password is not allowed on this instance", data):
+    print(data)
+    quit(1)
+
 issues=list(data.values())[-1]
 print(issues)
 
