320c64ae759c79d19d3709539ef16de23374bb7f
Your Name <you@example.com>
2020-06-11 04:25:49 +0800

    ex-get-platform-info.c: corrected CL_DEVICE_MAX_WORK_GROUP_SIZE type

--- rocm/opencl/opencl-programming-guide/ex-get-platform-info.c
+++ rocm/opencl/opencl-programming-guide/ex-get-platform-info.c
@@ -77,7 +77,7 @@ int main(int argc, char ** argv)
         DEVINFO_UINT, \    
         DEVINFO_SIZE_T, \    
         DEVINFO_UINT, \    
-        DEVINFO_UINT, \    
+        DEVINFO_SIZE_T, \    
         DEVINFO_USHORT, \    
         DEVINFO_STRING, \    
 	DEVINFO_SIZE_T \
