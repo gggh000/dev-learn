3b23a8bb9baa2de1fa8f90537d1c8f791d3e0d1c
Your Name <you@example.com>
2020-06-26 23:22:02 -0700

    p71: include book.h compile OK

--- cuda/windows/p71/p71/kernel.cu
+++ cuda/windows/p71/p71/kernel.cu
@@ -1,7 +1,7 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include "book.h"
 #include <stdio.h>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
