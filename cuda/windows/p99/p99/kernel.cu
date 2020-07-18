a3961cdac268e4a2c8824314d323e9d25897f457
Your Name <you@example.com>
2020-07-18 12:45:35 -0700

    opengl project1 added

--- cuda/windows/p99/p99/kernel.cu
+++ cuda/windows/p99/p99/kernel.cu
@@ -6,118 +6,103 @@
 #include "book.h"
 //#include "cpu_bitmap.h"
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+#define rnd(x) (x*rand()/ RAND_MAX)
+#define SPHERES 20
+#define INF 2e10f
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+struct Sphere {
+	float r, g, b;
+	float radius;
+	float x, y, z;
+
+	// hit functions. If ox, oy given coordinate has distance from the x,y center of sphere which is 
+	// smaller than its radius, we get hit, in that case, calclulate distance sqrt((dz) from r^2 - (dx^2+dy^2))
+	// and return dz + z, that z-coordinate of spheres center plus distance. 
+
+	__device__ float hit(float ox, float oy, float *n){ 
+		float dx = ox - x;
+		float dy = oy - y;
+		if (dx * dx + dy * dy < radius * radius) {
+			float dz = sqrtf(radius * radius - dx * dx - dy * dy);
+			*n = dz / sqrtf(radius * radius);
+			return dz + z;
+		}
+		return -INF;
+	}
+};
+
+Sphere * s;
+
+
+__global__ void kernel(unsigned char * s) {
+	// map from threadIdx/blockIdx to pixel positions.
+
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y + blockDim.x * gridDim.x;
+	float ox = (x - DIM / 2);
+	float oy = (y - DIM / 2);
+
+	float r = 0, g = 0, b = 0;
+	float maxz = -INF;
+	for (int i = 0; i < SPHERES; i++) {
+		if (t > maxz) {
+			float fscale = n;
+			r = s[i].r * fscale;
+			g = s[i].g * fscale;
+			b = s[i].b * fscale;
+			maxz = t;
+		}
+	}
+
+}
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
 }
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	// capture the start time.
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	CudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(&start, 0);
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	CPUBitmap bitmap(DIM, DIM);
+	unsigned char *dev_bitmap;
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	// alloc memory on the GPU for output bitmap.
 
-    return 0;
-}
+	cudaMalloc((void**)&dev_bitmap, bitmap.image_size());
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
+	// alloc memory for sphere dataset.
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
+	cudaMalloc((void**)&s, sizeof(Sphere) * SPHERES);
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	// alloc temp memory, initialize it, copy to memory on the GPU,
+	// and then free our temp memory.
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	Sphere * temp_s = (Sphere *)malloc(sizeof(Sphere) * SPHERES);
+	for (int i = 0; i < SPHERES; i++) {
+		temp_s[i].r = rnd(1.0f);
+		temp_s[i].g = rnd(1.0f);
+		temp_s[i].b = rnd(1.0f);
+		temp_s[i].x = rnd(1000.0f) - 500;
+		temp_s[i].y = rnd(1000.0f) - 500;
+		temp_s[i].z = rnd(1000.0f) - 500;
+		temp_s[i].radius = rnd(100.0f) + 20;
+	}
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	cudaMemcpy(s, temp_s, sizeof(Sphere) * SPHERES, cudaMemcpyHostToDevice);
+	free(temp_s);
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	// generate bitmap from our sphere sets
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+	dim3 grids(DIM / 16, DIM / 16);
+	dim3 threads(16, 16);
+	kernell << <grids, threads >> > (dev_bitmap);
+	cudaMemcpy(bitmap.get_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost);
+	bitmap.display_and_exit();
+	cudaFree(dev_bitmap);
+	cudaFree(s);
 }
