92876ccc00570f2ae59fbd67612a812ed44d1856
Your Name <you@example.com>
2020-07-25 14:16:28 -0700

    p46.cpp: option to build from source or file.

--- rocm/opencl/opencl-programming-guide/opencl11-book/p46.cpp
+++ rocm/opencl/opencl-programming-guide/opencl11-book/p46.cpp
@@ -5,8 +5,17 @@
 #include <chrono>
 #include <fstream>
 #define ARRAY_SIZE 1024 
-
+#define BUILD_FROM_FILE 0
 using namespace std;
+
+// A simple kernelfcn kernel
+const char *source =
+
+"kernel void kernelfcn(     global uint *dev_c, global uint *dev_a, global uint *dev_b)  \n"
+"{                                                                      \n"
+" uint tid = get_global_id(0);                                          \n"
+" dev_c[tid] = dev_a[tid] + dev_b[tid];                                 \n"
+"};\n";
 
 int main(int argc, char ** argv) {
     cl_context context = 0;
@@ -19,10 +28,8 @@ int main(int argc, char ** argv) {
     cl_int ret;
 
     cl_uint CONFIG_MAX_PLATFORMS=20;
-    cl_uint CONFIG_MAX_DEVICES=10;
     cl_platform_id platforms[CONFIG_MAX_PLATFORMS];
     cl_uint platforms_available;
-    cl_device_id devices[CONFIG_MAX_DEVICES];
 
     clGetPlatformIDs(CONFIG_MAX_PLATFORMS, platforms, &platforms_available );
     printf("\nNo. of platforms available: %d.\n", platforms_available);
@@ -33,17 +40,19 @@ int main(int argc, char ** argv) {
 
     // 2. Find a gpu/cpu device.
 
+    cl_uint CONFIG_MAX_DEVICES=10;
     cl_uint devices_available;
+    cl_device_id devices[CONFIG_MAX_DEVICES];
 
     ret  = clGetDeviceIDs( platforms[0], CL_DEVICE_TYPE_ALL, CONFIG_MAX_DEVICES, devices, &devices_available);
     printf("No. of devices available: %d.\n", devices_available);
-    device = devices[0];
+    // device = devices[0];
 
     cout << "device ID 1st device: " << device << endl;
 
     // Create an opencl context on first available platform
 
-    context = clCreateContext(NULL, 1, &device, NULL, NULL, &ret);
+    context = clCreateContext(NULL, 1, (const cl_device_id* )&devices[0], NULL, NULL, &ret);
 
     if (context == NULL ) {
         cerr << "Failed to create opencl context." << endl;
@@ -52,33 +61,38 @@ int main(int argc, char ** argv) {
 
     // Create a command queue on the first device available
 
-    commandQueue = clCreateCommandQueue(context, device, 0, &ret);
+    commandQueue = clCreateCommandQueue(context, devices[0], 0, &ret);
     
     if (commandQueue == NULL ) { 
        //Cleanup(context, commandQueue, program, kernel, memObjects);
        return 1;
     }
 
-    // Create opencl program from helloworld.cl kernel source
+    if (BUILD_FROM_FILE==1) {
+        // Create opencl program from helloworld.cl kernel source
 
-    ifstream myFile;
-    myFile.open ("HelloWorld.cl");
-    string str;
-    string src = "";
-    
-    while (getline(myFile, str))
-        src +=  str;
-    
+        ifstream myFile;
+        myFile.open ("HelloWorld.cl");
+        string str;
+        string src = "";
+        
+        while (getline(myFile, str))
+            src +=  str;
 
-    program = clCreateProgramWithSource(context, 1, (const char**)&src, (const size_t*)src.length(), &ret);
+        program = clCreateProgramWithSource(context, 1, (const char**)&src, (const size_t*)src.length(), &ret);
 
-    if (program == NULL) { 
-       //Cleanup(context, commandQueue, program, kernel, memObjects);
+        if (program == NULL) { 
+           //Cleanup(context, commandQueue, program, kernel, memObjects);
+        }
+
+        // Create opencl kernel.
+
+        cout << "Building kernel from file...";
+        kernel = clCreateKernel(program, "hello_kernel", &ret);
+    } else { 
+        cout << "Building kernel from source string...";
+        kernel = clCreateKernel(program, "kernel_fcn", &ret);
     }
-
-    // Create opencl kernel.
-
-    kernel = clCreateKernel(program, "hello_kernel", &ret);
 
     if (kernel == NULL) {
         cerr << "Failed to create a kernel " << endl;
@@ -101,6 +115,10 @@ int main(int argc, char ** argv) {
     cl_mem dev_memObjects = clCreateBuffer( context, CL_MEM_READ_WRITE, ARRAY_SIZE * sizeof(cl_uint), NULL, NULL );
     cl_mem dev_a = clCreateBuffer( context, CL_MEM_READ_WRITE, ARRAY_SIZE * sizeof(cl_uint), NULL, NULL );
     cl_mem dev_b  = clCreateBuffer( context, CL_MEM_READ_WRITE, ARRAY_SIZE * sizeof(cl_uint), NULL, NULL );
+
+    ret = clEnqueueWriteBuffer(commandQueue, dev_a, CL_TRUE, 0, ARRAY_SIZE * sizeof(cl_uint), a, NULL, NULL, NULL);
+    ret = clEnqueueWriteBuffer(commandQueue, dev_b, CL_TRUE, 0, ARRAY_SIZE * sizeof(cl_uint), b, NULL, NULL, NULL);
+    //ret = clEnqueueWriteBuffer(commandQueue, dev_memObjects, CL_TRUE, 0, ARRAY_SIZE * sizeof(cl_uint), memObjects, NULL, NULL, NULL);
 
     // copy from a/b/memObjects to dev_a/dev_b/dev_memObjets.!!!!
 
