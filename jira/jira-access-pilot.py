92f65bcc5d326b7defca42e966991531886a5399
Your Name <you@example.com>
2020-08-13 00:06:09 -0400

    jira-access-pilot.py: conditionalization test OK.

--- jira/jira-access-pilot.py
+++ jira/jira-access-pilot.py
@@ -14,20 +14,23 @@ CONFIG_ENABLE_NAT=1
 if (CONFIG_ENABLE_NAT == 0):
 	JIRA_SERVER_IP={\
 		"AMD_LOCAL": "https://10.217.73.243:8080", \
-		"AMD_LOCAL_IXT39": "http://10.216.67.167:8080" \
+		"AMD_LOCAL_IXT39": "http://10.216.67.167:8080", \
+        "HOME_LOCAL": "http://11.0.0.29:8080" \
 	}
 else:
 	JIRA_SERVER_IP={\
 		"AMD_LOCAL": "https://10.217.73.243:8080", \
-		"AMD_LOCAL_IXT39": "http://192.168.122.200:8080" \
+		"AMD_LOCAL_IXT39": "http://192.168.122.200:8080", \
+        "HOME_LOCAL": "HTTP://11.0.0.29:8080" \
 	}
 
 JIRA_SERVER_AUTH_BASIC={\
 	"AMD_LOCAL": "ggjira200, 8981555aaa", \
-	"AMD_LOCAL_IXT39": "ggjira300, 8981555aaa" \
+	"AMD_LOCAL_IXT39": "ggjira100, 8981555aaa", \
+	"HOME_LOCAL": "ggjira300, 8981555aaa" \
 	}
 
-JIRA_SERVER_PROFILE="AMD_LOCAL_IXT39"
+JIRA_SERVER_PROFILE="HOME_LOCAL"
 print("Loggin into: ", JIRA_SERVER_IP[JIRA_SERVER_PROFILE])
 print("Logging with: ", \
 	JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[0].strip(), "|", \
@@ -35,47 +38,48 @@ print("Logging with: ", \
 options = {"server": "https://" + JIRA_SERVER_IP[JIRA_SERVER_PROFILE]}
 
 
-'''
-jira = JIRA('https://' + JIRA_SERVER_IP[JIRA_SERVER_PROFILE], basic_auth=(\
-        JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[0].strip(), \
-        JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[1].strip()))
-'''
-
 if (CONFIG_JIRA_LIBRARY==CONFIG_JIRA_LIBRARY_ATLASSIAN):
 
     print("Using atlassian library...")
 
     jira = Jira(
-        url='http://192.168.122.200:8080',
-        username='ggjira300',
-        password='8981555aaa')
+        url=JIRA_SERVER_IP[JIRA_SERVER_PROFILE],
+        username=JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[0].strip(),
+        password=JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[1].strip())
 
     #props = jira.application_properties()
     #projects = jira.projects()
     
     JQL = 'project = gg-proj-000 AND status IN ("To Do", "In Progress") ORDER BY issuekey'
     data = jira.jql(JQL)
-    
+
     # print(data)
     print(type(data))
+
+    '''
     for i in range(0, len(data)):
             print("=")
             print(list(data.keys())[i])
             print("-")
             print(list(data.values())[i])
     
-    '''
     issues=list(data.values())[-1]
     print(issues)
-    
+
     for i in issues:
-            pprint(i)
-    
+        pprint(i)
     '''
+    
     
 elif (CONFIG_JIRA_LIBRARY==CONFIG_JIRA_LIBRARY_JIRA):
     print("Using jira library")
     
+    '''
+    jira = JIRA('https://' + JIRA_SERVER_IP[JIRA_SERVER_PROFILE], basic_auth=(\
+        JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[0].strip(), \
+        JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[1].strip()))
+    '''
+
     jira = JIRA(basic_auth=(\
 	    JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[0].strip(), \
 	    JIRA_SERVER_AUTH_BASIC[JIRA_SERVER_PROFILE].split()[1].strip()))
