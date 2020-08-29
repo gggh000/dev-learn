2452428e90651d386770f86acee12f478b0e7ac9
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-08-29 14:36:11 -0400

    checkpoint, build works on ubuntu 1804, kernel 4.x

--- device-drivers/main.c
+++ device-drivers/main.c
@@ -1,11 +1,7 @@
-#ifndef _LINUX_INIT_H
-#define _LINUX_INIT_H
-
 #include <linux/module.h>
 #include <linux/init.h>
-//#include <linux/kernel.h>
 
-MODULE_LICENSE("Dual BSD/GPL")
+MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void) {
     printk(KERN_ALERT "Hello, world.\n");
@@ -19,5 +15,3 @@ static void hello_exit(void) {
 
 module_init(hello_init);
 module_exit(hello_exit);
-
-#endif
