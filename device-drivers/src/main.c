18cc9345c08f400d124d1e6dd505ca292b888a15
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-09-04 04:44:49 -0400

    checkpoint

--- device-drivers/src/main.c
+++ device-drivers/src/main.c
@@ -1,10 +1,20 @@
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/stat.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+int myint=3;
+module_param(myint, int, 0);
+
+MODULE_PARM_DESC(myint, "An integer");
+//module_param(myint, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
 static int hello_init(void) {
     printk(KERN_ALERT "Hello, world.\n");
+    printk(KERN_INFO "myint is an integer: %d\n", myint);
     return 0;
 }
 
