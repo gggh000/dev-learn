c033f6e3e3739514f776352e17cc354040338d23
Your Name <you@example.com>
2020-09-05 10:17:26 -0700

    scull: main.c: scull_open compile OK.

--- device-drivers/src/main.c
+++ device-drivers/src/main.c
@@ -4,6 +4,8 @@
 #include <linux/kernel.h>
 #include <linux/stat.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/cdev.h>
 
 #include "scull.h"
 
@@ -16,6 +18,53 @@ module_param(myint, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 int scull_major =   SCULL_MAJOR;
 int scull_minor =   0;
 int scull_nr_devs = SCULL_NR_DEVS;  /* number of bare scull devices */
+int scull_quantum = SCULL_QUANTUM;
+int scull_qset =    SCULL_QSET;
+
+int scull_trim(struct scull_dev *dev)
+{
+    struct scull_qset *next, *dptr;
+    int qset = dev->qset;   /* "dev" is not-null */
+    int i;
+
+    for (dptr = dev->data; dptr; dptr = next) { /* all the list items */
+        if (dptr->data) {
+            for (i = 0; i < qset; i++)
+                kfree(dptr->data[i]);
+            kfree(dptr->data);
+            dptr->data = NULL;
+        }
+        next = dptr->next;
+        kfree(dptr);
+    }
+    dev->size = 0;
+    dev->quantum = scull_quantum;
+    dev->qset = scull_qset;
+    dev->data = NULL;
+    return 0;
+}
+
+int scull_open(struct inode  * inode, struct file * filp) {
+    struct scull_dev * dev;
+    dev = container_of(inode->i_cdev, struct scull_dev, cdev);
+    filp->private_data = dev;
+    
+    if (( filp->f_flags & O_ACCMODE ) == O_WRONLY) {
+        scull_trim(dev);
+    }
+    return 0;
+}
+
+
+struct file_operations scull_fops = {
+    .owner =    THIS_MODULE,
+    //.llseek =   scull_llseek,
+    //.read =     scull_read,
+    //.write =    scull_write,
+    //.unlocked_ioctl = scull_ioctl,
+    .open =     scull_open,
+    //.release =  scull_release,
+};
 
 static int hello_init(void) {
     printk(KERN_ALERT "Hello, world.\n");
@@ -51,3 +100,5 @@ static void hello_exit(void) {
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+
