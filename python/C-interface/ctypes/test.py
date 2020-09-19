88f084e00e0121b71853aa6308c9bbbb526f2bb5
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-09-19 05:21:04 -0400

    basic_function_helper.py: exit on exception.

---
+++ python/C-interface/ctypes/test.py
@@ -0,0 +1,3 @@
+from basic_function_helper import do_square_using_c
+my_list = np.arange(1000)
+squared_list = do_square_using_c(*my_list)
---
+++ python/C-interface/ctypes/theories/build.sh
@@ -0,0 +1,1 @@
+gcc -o basic_function_linux.so -shared -fPIC -O2 basic_function.c 
