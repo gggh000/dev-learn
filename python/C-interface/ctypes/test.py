939252a89afff24af803d4ed0563f33923cbcb80
Your Name <you@example.com>
2020-09-19 11:07:16 -0700

    test.py: pass array instead of pts to array, fixed

--- python/C-interface/ctypes/test.py
+++ python/C-interface/ctypes/test.py
@@ -2,4 +2,5 @@ import numpy as np
 from basic_function_helper import do_square_using_c
 #my_list = np.arange(1000)
 my_list = range(0, 10)
-squared_list = do_square_using_c(*my_list)
+squared_list = do_square_using_c(my_list)
+print(squared_list)
