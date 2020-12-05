8f282000c76609a4a15a78dd8dc97b1c805fcbd2
Your Name <you@example.com>
2020-12-05 02:42:46 -0800

    called function within asm OK

---
+++ c/asm-c-linkage/build.sh
@@ -0,0 +1,2 @@
+nasm -felf64 -F dwarf hello.asm && ld hello.o && ./a.out
+echo "use gdb a.out to start debugging"
--- c/asm-c-linkage/hello.asm
+++ c/asm-c-linkage/hello.asm
@@ -1,14 +1,28 @@
-   global    _start
+;   https://cs.lmu.edu/~ray/notes/nasmtutorial/
 
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+    global    _start
 
-          section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+            section   .text
+_start:     mov     rax, 1                  ; system call for write
+            mov     rdi, 1                  ; file handle 1 is stdout
+            mov     rsi, message            ; address of string to output
+            mov     rdx, 13                 ; number of bytes
+            syscall                         ; invoke operating system to do the write
+
+            mov edi,7                       ; pass a function parameter
+            call otherFunction              ; run the function below, until it does "ret"
+            add eax,1                       ; modify returned value
+
+            mov     rax, 60                 ; system call for exit
+            xor     rdi, rdi                ; exit code 0
+            syscall                         ; invoke operating system to exit
+
+; a function "declaration" in assembly
+
+otherFunction: 
+            mov     eax, edi ; return our function's only argument
+            ret
+
+            section   .data
+message:    db        "Hello, World", 10    ; note the newline at the end
+
