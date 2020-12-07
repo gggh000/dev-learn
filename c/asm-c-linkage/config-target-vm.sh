4f8c89ceac5b617aa8a670602009e753f3632a62
Your Name <you@example.com>
2020-12-07 21:42:12 +0000

    config-target-vm.sh: reconnect/disconnect host/target works

--- c/asm-c-linkage/config-target-vm.sh
+++ c/asm-c-linkage/config-target-vm.sh
@@ -8,15 +8,34 @@
 P1=$1
 VM_NAME=minix-boot
 IMAGE_NAME=/var/lib/libvirt/images/minix-boot-1.qcow2
+DISK_NAME=hdc
 
 if [[ $P1 == "rm" ]] ; then
 	echo "removing second hdd"
-	virsh detach-disk --domain $VM_NAME $IMAGE_NAME --persistent --config --live
+	virsh destroy $VM_NAME
+	virsh list
+	#sleep 5
+	virsh detach-disk --domain $VM_NAME $DISK_NAME --persistent --config
 	virsh domblklist $VM_NAME
+	echo "attaching/mounting qcow on host..."
+	echo "sd devices before connect..."
+	ls -l | grep nbd
+	modprobe nbd max_part=8
+	qemu-nbd --connect=/dev/nbd0 $IMAGE_NAME
+	echo "sd devices after connect..."
+	ls -l | grep nbd
+	
 elif [[ $P1 == "add" ]] ; then
+	echo "disconnect sd device..."
+	qemu-nbd --disconnect /dev/nbd0
+	rmmod nbd
 	echo "attaching second hdd"
-	virsh attach-disk --domain $VM_NAME --source $IMAGE_NAME --target hdc --config --live
+	virsh attach-disk --domain $VM_NAME --source $IMAGE_NAME --target hdc --config
+	# detach during live vm running not working so following unnecessary unless detach part works.
+	#virsh attach-disk --domain $VM_NAME --source $IMAGE_NAME --target hdc --config --live
 	virsh domblklist $VM_NAME
+	virsh start $VM_NAME
+	virsh list
 else
 	echo "invalid parameter: $P1 "
 fi
