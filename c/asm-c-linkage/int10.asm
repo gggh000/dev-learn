70e1cbedd3fbcbec86b12cf344c0b109eb7c466f
Your Name <you@example.com>
2020-12-06 23:06:51 -0800

    checkpoint: print from MBR code works. use build1.sh which will produce a.out a1.out and write to mbr of /dev/sdb

--- c/asm-c-linkage/int10.asm
+++ c/asm-c-linkage/int10.asm
@@ -3,22 +3,22 @@
             section   .text
 _start:     
 ;	set video mode
-	mov	ax, 0x000d
+	mov	ax, 0x0002
 	int	0x10
 
 ; 	set cursor
-	mov 	ah, 0x2  		; set cursor function.
-	sub 	bh, bh			; select page.
-	mov 	dl, 10			; row 10.
-	sub	dh, dh			; col 0.
-	int 	0x10			; call
+;	mov 	ah, 0x2  		; set cursor function.
+;	sub 	bh, bh			; select page.
+;	mov 	dl, 10			; row 10.
+;	sub	dh, dh			; col 0.
+;	int 	0x10			; call
 
 ;	write 'A' 16 times at current cursor.
-	mov 	al, 'A'                 ; char 2 display.
-        mov     ah, 0x10                ; int 10h, write char.
-        sub     bh, bh                  ; page to write.
-	sub     bl, bl		    	; attrib: text mode.
-        mov     cx, 0x10                ; No. of times to write to screen.
+        mov     ah, 0x0e                ; int 10h, write char.
+	mov 	al, '#'                 ; char 2 display.
+        int     0x10
+        mov     ah, 0x0e                ; int 10h, write char.
+	mov 	al, '$'                 ; char 2 display.
         int     0x10
 	jmp     $
 
