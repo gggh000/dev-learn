36f739b225040609dc8980dab1ff53947eaacd39
Your Name <you@example.com>
2020-12-06 22:19:23 -0800

    disassembler can correctly disasm int10 service

--- c/asm-c-linkage/int10.asm
+++ c/asm-c-linkage/int10.asm
@@ -4,11 +4,8 @@
 _start:     mov     al, 'A'                 ; char 2 display.
             mov     ah, 0x10                ; int 10h, write char.
             sub     bh, bh                  ; page to write.
-            mov     cx, 10                  ; No. of times to write to screen.
+            mov     cx, 0x10                ; No. of times to write to screen.
             int     0x10
-
-            mov     rax, 60                 ; system call for exit
-            xor     rdi, rdi                ; exit code 0
-            syscall                         ; invoke operating system to exit
+	    jmp     $
 
             section   .data
