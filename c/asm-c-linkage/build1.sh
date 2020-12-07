1a4444817db830425bee881783e14ab541a47d5d
Your Name <you@example.com>
2020-12-06 22:53:28 -0800

    checkpoint

--- c/asm-c-linkage/build1.sh
+++ c/asm-c-linkage/build1.sh
@@ -1,7 +1,7 @@
 nasm -felf64 -F dwarf int10.asm
 ld int10.o
 echo "use gdb a.out to start debugging"
-tail -c $((1680-0x80)) a.out | head -c 446 > a1.out
-echo "code portion of a.out is written to a1.out."
-echo "use hexdump -C <filename> -n <No. of bytes> to display it."
-
+tail -c $((` wc -c a.out | cut -d ' ' -f1`-0x80)) a.out | head -c 446 > a1.out
+dd if=a1.out of=/dev/sdb
+hexdump -C /dev/sdb -n 512
+objdump -D -b binary -m i386 a1.out
--- c/asm-c-linkage/int10.asm
+++ c/asm-c-linkage/int10.asm
@@ -1,11 +1,25 @@
     global    _start
 
             section   .text
-_start:     mov     al, 'A'                 ; char 2 display.
-            mov     ah, 0x10                ; int 10h, write char.
-            sub     bh, bh                  ; page to write.
-            mov     cx, 0x10                ; No. of times to write to screen.
-            int     0x10
-	    jmp     $
+_start:     
+;	set video mode
+	mov	ax, 0x000d
+	int	0x10
 
-            section   .data
+; 	set cursor
+	mov 	ah, 0x2  		; set cursor function.
+	sub 	bh, bh			; select page.
+	mov 	dl, 10			; row 10.
+	sub	dh, dh			; col 0.
+	int 	0x10			; call
+
+;	write 'A' 16 times at current cursor.
+	mov 	al, 'A'                 ; char 2 display.
+        mov     ah, 0x10                ; int 10h, write char.
+        sub     bh, bh                  ; page to write.
+	sub     bl, bl		    	; attrib: text mode.
+        mov     cx, 0x10                ; No. of times to write to screen.
+        int     0x10
+	jmp     $
+
+        section   .data
