d295002db23b0066160060a4dcffc164e62d2b08
Your Name <you@example.com>
2020-12-10 10:26:25 +0000

    bootstrap code is written, untested

--- c/asm-c-linkage/int10.asm
+++ c/asm-c-linkage/int10.asm
@@ -6,13 +6,6 @@ _start:
 	mov	ax, 0x0002
 	int	0x10
 
-; 	set cursor
-;	mov 	ah, 0x2  		; set cursor function.
-;	sub 	bh, bh			; select page.
-;	mov 	dl, 10			; row 10.
-;	sub	dh, dh			; col 0.
-;	int 	0x10			; call
-
 ;	write 'A' 16 times at current cursor.
         mov     ah, 0x0e                ; int 10h, write char.
 	mov 	al, '#'                 ; char 2 display.
@@ -20,6 +13,28 @@ _start:
         mov     ah, 0x0e                ; int 10h, write char.
 	mov 	al, '$'                 ; char 2 display.
         int     0x10
-	jmp     $
+
+	mov	ah, 0x42		; bios 13h extended read service code.
+	mov	dl, 0x81		; drive No.
+
+;	DS:SI - pointer to DAP (disk access packet).
+
+	mov	ax, 0x7c00
+	mov	ds, ax
+	lea	si, [DAP]
+
+	int 	0x13			; issue the command.
+	
+	mov	ax, 0x8000
+	push 	ax
+	ret
+
+DAP:
+;	DAP packet for bios int 13h (ah=0x42)
+	db 	0x10			; size of this data struct.
+	db 	0x00			; unused.
+	dw	0x02			; No. of sectors to read.
+	dd	0x00008000		; segment:offset of target location in memory
+	dd	0x28			; not sure this needs to be inspected using ext2 on hdd not fdd.
 
         section   .data
