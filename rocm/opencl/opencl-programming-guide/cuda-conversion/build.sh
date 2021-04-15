352010c725c25d5eb516ba0b252d8debd990c466
Your Name <you@example.com>
2021-04-15 15:23:27 +0800

    CMakeLists.txt: had to include zlib include and lib dir to generate makefile.

--- rocm/opencl/opencl-programming-guide/cuda-conversion/build.sh
+++ rocm/opencl/opencl-programming-guide/cuda-conversion/build.sh
@@ -1,5 +1,10 @@
 mkdir build ; cd build
-cmake -DCMAKE_FIND_LIBRARY_PREFIXES=lib -DCMAKE_FIND_LIBRARY_SUFFIXES=.so ..
+cmake \
+	-DCMAKE_FIND_LIBRARY_PREFIXES=lib \
+	-DCMAKE_FIND_LIBRARY_SUFFIXES=.so \
+	-DZLIB_INCLUDE_DIR=/usr/include \
+	-DZLIB_LIBRARY=/usr/lib/x86_64-linux-gnu/ \
+	.. 
 make p41
 make p25-cuda
 make p189
