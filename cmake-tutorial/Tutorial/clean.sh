54d13bcf6d5f9dd838a05326a21e10f4f46fcc41
Your Name <you@example.com>
2021-04-17 14:39:17 +0800

    clean.sh: improvization

--- cmake-tutorial/Tutorial/clean.sh
+++ cmake-tutorial/Tutorial/clean.sh
@@ -2,6 +2,11 @@ rm -rf CMakeFiles
 rm -rf CMakeCache.txt
 rm -rf cmake_install.cmake
 rm -rf ./build/*
-rm -rf  TutorialConfig.h
-rm -rf MakeFile
+rm -rf TutorialConfig.h
+rm -rf Makefile
+rm -rf MathFunctions/CMakeFiles/
+rm -rf MathFunctions/Makefile
+rm -rf MathFunctions/cmake_install.cmake
+rm -rf MathFunctions/libMathFunctions.a
 ls -l
+
