4c58b30798ad2288ef0d870103f16ba1eb328954
Your Name <you@example.com>
2021-05-20 00:38:32 -0700

    code cleanup

--- cuda/linux/cuda-by-example/p61/p61.cu
+++ cuda/linux/cuda-by-example/p61/p61.cu
@@ -8,14 +8,11 @@
 // The trick is describe in p65 to use formula (N+127) / 128 for blocknumbers so that when block number starts from 1, it is 
 // (1+127) / 128.
 
-#define N 1024
+#define N 2048
 #define MAX_THREAD_PER_BLOCK 1024
-__global__ void add( int * a, int * b, int * c ) {
-//	int tid = threadIdx.x + blockIdx.x * blockDim.x;
-	int tid = threadIdx.x;
 
-//	while (tid < N)
-//	tid += blockDim.x * gridDim.x;
+__global__ void add( int * a, int * b, int * c ) {
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
     if (tid < N)
        	c[tid] = a[tid] + b[tid];
@@ -46,11 +43,9 @@ int main (void) {
 	// invoke the kernel: 
 	// block count: (N+127)/128
 	// thread count: 128
-
     
     printf("\nLaunch cuda kernel...");
-	add<<<(N+MAX_THREAD_PER_BLOCK-1)/MAX_THREAD_PER_BLOCK, MAX_THREAD_PER_BLOCK>>> (dev_a, dev_b, dev_c);
-	//add<<<1, N>>> (dev_a, dev_b, dev_c);
+    add<<<(N+MAX_THREAD_PER_BLOCK-1)/MAX_THREAD_PER_BLOCK, MAX_THREAD_PER_BLOCK>>> (dev_a, dev_b, dev_c);
 
     printf("\nCopy back from GPU to host...");
 	cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
