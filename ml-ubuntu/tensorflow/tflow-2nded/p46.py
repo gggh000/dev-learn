07dc777ec67d6e6f5651c9ee6b83c1d8d25934c5
Your Name <you@example.com>
2021-06-15 22:34:20 -0700

    p46.py: checkpoint, p52

--- ml-ubuntu/tensorflow/tflow-2nded/p46.py
+++ ml-ubuntu/tensorflow/tflow-2nded/p46.py
@@ -3,7 +3,8 @@ import tarfile
 import pandas as pd
 import urllib.request
 import matplotlib.pyplot as plt
-
+import numpy as np
+ENABLE_PLOT=0
 DOWNLOAD_ROOT="http://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH=os.path.join("datasets", "housing")
 HOUSING_URL=DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
@@ -16,6 +17,21 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
+#p52
+def split_train_test(data, test_ratio):
+    DEBUG = 1
+    shuffled_indices = np.random.permutation(len(data))
+
+    if DEBUG:
+        print("shuffled_indices: ", shuffled_indices)
+
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]    
+
+
+#p52-end
 print("housing: ", HOUSING_PATH)
 fetch_housing_data()
 
@@ -29,6 +45,10 @@ housing=load_housing_data()
 print(housing.info())
 print(housing["ocean_proximity"].value_counts())
 print(housing.describe())
-housing.hist(bins=50, figsize=(20,15))
-plt.show()
 
+if ENABLE_PLOT:
+    housing.hist(bins=50, figsize=(20,15))
+    plt.show()
+
+train_set, test_set=split_train_test(housing, 0.2)
+print(len(train_set), len(test_set))
