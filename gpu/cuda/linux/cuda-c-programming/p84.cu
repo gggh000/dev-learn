3df73fd8cde3a86a9c685d79c9d06251670c647f
Your Name <you@example.com>
2021-06-18 22:13:12 -0700

    p84: improvement in code

--- gpu/cuda/linux/cuda-c-programming/p84.cu
+++ gpu/cuda/linux/cuda-c-programming/p84.cu
@@ -2,7 +2,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <cuda_runtime.h>
-
+#define ENABLE_34 1
 inline double seconds()
 {
     struct timeval tp;
@@ -23,34 +23,6 @@ __global__ void warmingUp(float *c)
         ib = 200.0f;
     }
 
-    c[tid] = ia + ib;
-}
-
-__global__ void mathKernel4(float *c)
-{
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    float ia, ib;
-    ia = ib = 0.0f;
-
-    int itid = tid >> 5;
-
-    if (itid & 0x01 == 0) {
-        ia = 100.0f;
-    } else {
-        ib = 200.0f;
-    }
-
-    c[tid] = ia + ib;
-}
-
-__global__ void mathKernel3(float * c) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    float ia, ib;
-    ia = ib = 0.0f;
-    
-    bool ipred = (tid % 2 == 0);
-    if (ipred)  ia = 100.0f; 
-    if (!ipred) ib = 200.0f; 
     c[tid] = ia + ib;
 }
 
@@ -78,6 +50,34 @@ __global__ void mathKernel2(float * c) {
         b = 200.0f;
     }
     c[tid] = a + b;
+}
+
+__global__ void mathKernel3(float * c) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    float ia, ib;
+    ia = ib = 0.0f;
+    
+    bool ipred = (tid % 2 == 0);
+    if (ipred)  ia = 100.0f; 
+    if (!ipred) ib = 200.0f; 
+    c[tid] = ia + ib;
+}
+
+__global__ void mathKernel4(float *c)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    float ia, ib;
+    ia = ib = 0.0f;
+
+    int itid = tid >> 5;
+
+    if (itid & 0x01 == 0) {
+        ia = 100.0f;
+    } else {
+        ib = 200.0f;
+    }
+
+    c[tid] = ia + ib;
 }
 
 int main(int argc, char **argv) {
@@ -120,40 +120,46 @@ int main(int argc, char **argv) {
     warmingUp<<<grid,block>>>(d_C);
     cudaDeviceSynchronize();
     iElaps = seconds() - iStart;
-    printf("warmup <<< %d %d >>> elapsed %06d sec.\n", grid.x, block.x, iElaps);
+    //printf("warmup <<< %d %d >>> elapsed %d sec.\n", grid.x, block.x, iElaps);
 
     // run kernel 1.
+    // tid%2 causes even numbered threads to take "if" clause and off numbered threads to take "else" clause.
+    // 
 
     iStart = seconds();
     mathKernel1<<<grid, block>>>(d_C);
     cudaDeviceSynchronize();
     iElaps = seconds() - iStart;
-    printf("mathKernel1 <<< %4d %4d >>> elapsed %06d sec.\n", grid.x, block.x, iElaps);
+    printf("mathKernel1 <<< %4d %4d >>> elapsed %d sec.\n", grid.x, block.x, iElaps);
 
     // run kernel 2.
+    //(tid/warpsize)%2 == 0 test causes branch granularity to multiple of warpsize. 
 
     iStart = seconds();
     mathKernel2<<<grid, block>>>(d_C);
     cudaDeviceSynchronize();
     iElaps = seconds() - iStart;
-    printf("mathKernel2 <<< %4d %4d >>> elapsed %06d sec.\n", grid.x, block.x, iElaps);
+    printf("mathKernel2 <<< %4d %4d >>> elapsed %d sec.\n", grid.x, block.x, iElaps);
 
-    // run kernel 3.
+    if (ENABLE_34 == 1) {
 
-    iStart = seconds();
-    mathKernel3<<<grid, block>>>(d_C);
-    cudaDeviceSynchronize();
-    iElaps = seconds() - iStart;
-    printf("mathKernel3 <<< %4d %4d >>> elapsed %06d sec.\n", grid.x, block.x, iElaps);
-
-    // run kernel 4.
-
-    iStart = seconds();
-    mathKernel4<<<grid, block>>>(d_C);
-    cudaDeviceSynchronize();
-    iElaps = seconds() - iStart;
-    printf("mathKernel4 <<< %4d %4d >>> elapsed %06d sec.\n", grid.x, block.x, iElaps);
-
+        // run kernel 3.
+    
+        iStart = seconds();
+        mathKernel3<<<grid, block>>>(d_C);
+        cudaDeviceSynchronize();
+        iElaps = seconds() - iStart;
+        printf("mathKernel3 <<< %4d %4d >>> elapsed %d sec.\n", grid.x, block.x, iElaps);
+    
+        // run kernel 4.
+    
+        iStart = seconds();
+        mathKernel4<<<grid, block>>>(d_C);
+        cudaDeviceSynchronize();
+        iElaps = seconds() - iStart;
+        printf("mathKernel4 <<< %4d %4d >>> elapsed %d sec.\n", grid.x, block.x, iElaps);
+	}
+	
     cudaFree(d_C);
     cudaDeviceReset();
     return 0;
--- gpu/cuda/linux/cuda-c-programming/run-p84.sh
+++ gpu/cuda/linux/cuda-c-programming/run-p84.sh
@@ -1,4 +1,5 @@
 FILENAME=p84
+BAR="----------------------------------"
 OUTPUT=$FILENAME.out
 nvcc -g -G -arch=sm_20 $FILENAME.cu -o $OUTPUT
 #echo "BRANCH OCCUPANCY:"
@@ -6,6 +7,16 @@ nvcc -g -G -arch=sm_20 $FILENAME.cu -o $OUTPUT
 #echo "BRANCH EFFICIENCY:"
 #ncu --metrics smsp__sass_average_branch_targets_threads_uniform.pct ./$OUTPUT
 
+function printWithBar() {
+echo $BAR
+echo $1
+echo $BAR 
+}
+
+printWithBar "--events branch,divergent_branch"
 nvprof --events branch,divergent_branch  ./$OUTPUT $1 $2
+printWithBar "--metrics gld_throughput"
 nvprof --metrics gld_throughput ./$OUTPUT $1 $2
+printWithBar "--metrics branch_efficiency"
 nvprof --metrics branch_efficiency ./$OUTPUT $1 $2
+
