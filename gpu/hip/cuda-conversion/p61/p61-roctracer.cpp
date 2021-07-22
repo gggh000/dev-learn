6392ae92aec6886721c3387671af698001aee00e
Your Name <you@example.com>
2021-07-22 11:31:35 -0700

    feedforward.py, neuron.py: added

--- gpu/hip/cuda-conversion/p61/p61-roctracer.cpp
+++ gpu/hip/cuda-conversion/p61/p61-roctracer.cpp
@@ -252,8 +252,9 @@ int main (void) {
 	// allocate dev memory for N size for pointers declared earlier.
 
     printf("\nAllocating memory...(size %u array size of INT).\n", N );
-    init_tracing();
-    start_tracing();
+    roctxMark("Allocating memory");
+    //init_tracing();
+    //start_tracing();
 
     a = (int*)malloc(N * sizeof(int));
     b = (int*)malloc(N * sizeof(int));
@@ -268,12 +269,15 @@ int main (void) {
 		c[i] = 999;
 	}
 
+    roctxMark("Copy");
 	// copy the initialized local memory values to device memory. 
 
     printf("\nCopy host to device...");
+    roctxRangePush("Memcopy host to dev");
 	hipMemcpy(dev_a, a, N * sizeof(int), hipMemcpyHostToDevice);
 	hipMemcpy(dev_b, b, N * sizeof(int), hipMemcpyHostToDevice);
 	hipMemcpy(dev_c, c, N * sizeof(int), hipMemcpyHostToDevice);
+    roctxRangePop();
 
     const unsigned blocks = 512;
     const unsigned threadsPerBlock = 256;
@@ -282,10 +286,14 @@ int main (void) {
 	// block count: (N+127)/128
 	// thread count: 128
     
+    roctxRangePush("Launch kernel");
     hipLaunchKernelGGL(add, blocks, threadsPerBlock, 0, 0, dev_a, dev_b, dev_c);
+    roctxRangePop();
+    roctxRangePush("Memcopy dev to host");
     hipMemcpy(a, dev_a, N * sizeof(int), hipMemcpyDeviceToHost);
     hipMemcpy(b, dev_b, N * sizeof(int), hipMemcpyDeviceToHost);
     hipMemcpy(c, dev_c, N * sizeof(int), hipMemcpyDeviceToHost);
+    roctxRangePop();
 
     stepSize =  N /20 ;	
 	for (int i = 0; i < N; i+=stepSize) {
@@ -298,6 +306,6 @@ int main (void) {
     free(a);
     free(b);
     free(c);
-    stop_tracing();
+    //stop_tracing();
     return 0;
 }
