9cfee0c6762996ead0728e925854f8a945fc8611
Your Name <you@example.com>
2021-07-22 11:33:43 -0700

    feedforward.py: missing code, added

--- ml-ubuntu/neural-network/feedforward.py
+++ ml-ubuntu/neural-network/feedforward.py
@@ -1,6 +1,22 @@
 import numpy as np
 
-# ... code from previous section here
+def sigmoid(x):
+
+  # Our activation function: f(x) = 1 / (1 + e^(-x))
+
+  return 1 / (1 + np.exp(-x))
+
+class Neuron:
+  def __init__(self, weights, bias):
+    self.weights = weights
+    self.bias = bias
+
+  def feedforward(self, inputs):
+
+    # Weight inputs, add bias, then use the activation function
+
+    total = np.dot(self.weights, inputs) + self.bias
+    return sigmoid(total)
 
 class OurNeuralNetwork:
   '''
