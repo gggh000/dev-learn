08008a7d7d914e4ea3775d7a51b6d00c490d5e4f
Your Name <you@example.com>
2021-07-27 16:55:15 -0700

    p297.py: added, compiling ok

--- gpu/tflow/tensorflow/tflow-2nded/p297.py
+++ gpu/tflow/tensorflow/tflow-2nded/p297.py
@@ -2,3 +2,24 @@ import tensorflow as tf
 from tensorflow import keras
 print(tf.__version__)
 print(keras.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+print("X_train_full.shape: ", X_train_full.shape)
+print("X_train_full.dtype: ", X_train_full.dtype)
+
+X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:]/255.0
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test - X_test / 255.0
+class_names = ["T-shirt/top","Trouser", "Pullover", "Dress", "Coat" , "Sandal", "Shirt", "Sneaker","Bad","Ankle boot"]
+
+model=keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape = [28, 28]))
+model.add(keras.layers.Dense(300, activation="relu"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(30, activation="softmax"))
+
+print("model summary: ", model.summary)
+
+
+
