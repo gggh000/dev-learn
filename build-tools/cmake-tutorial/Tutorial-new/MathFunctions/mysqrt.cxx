5e6e2ddbee2b9103a14d4db496c64831276af2af
Your Name <you@example.com>
2021-08-24 21:45:57 -0700

    cmake-study: tutorial-new: step2, partially OK, without conditional compiling

---
+++ build-tools/cmake-tutorial/Tutorial-new/MathFunctions/mysqrt.cxx
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double mysqrt(double pval) { 
+    cout << "mysqrt: entered." << endl;
+    return pow(pval, 0.5) ; 
+}
--- build-tools/cmake-tutorial/Tutorial-new/tutorial.cxx
+++ build-tools/cmake-tutorial/Tutorial-new/tutorial.cxx
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <TutorialConfig.h>
+#include <MathFunctions.h>
 
+using namespace std;
 int main(int argc, char *argv[]) {
    if (argc < 2) {
 
         // report version.
 
-        const double inputValue = std::stod(argv[1]);
         std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
                             << Tutorial_VERSION_MINOR << std::endl;
         std::cout << "Usage: " << argv[0] << " number" << std::endl;
         return 1;
     }
+    const double inputValue = std::stod(argv[1]);
+    const double outputValue = mysqrt(inputValue);
+    cout << "output: " << outputValue << endl;
+    return 0;
 }
