a849bf0c00cffb08303dee7720660543ee7398cc
Your Name <you@example.com>
2021-08-26 18:02:42 -0700

    s5. adding system introspection

--- build-tools/cmake-tutorial/Tutorial-redo/MathFunctions/mysqrt.cxx
+++ build-tools/cmake-tutorial/Tutorial-redo/MathFunctions/mysqrt.cxx
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <math.h>
+//#include <cmath.h>
 
 using namespace std;
 
-double mysqrt(double pval) { 
-    cout << "mysqrt: entered." << endl;
-    return pow(pval, 0.5) ; 
+double mysqrt(double x) { 
+    
+    #if defined(HAVE_LOG) && defined(HAVE_EXP)
+      double result = exp(log(x) * 0.5);
+      std::cout << "Computing sqrt of " << x << " to be " << result
+                << " using log and exp" << std::endl;
+    #else
+      double result = x;
+    #endif
+
+    return result;
+        
 }
