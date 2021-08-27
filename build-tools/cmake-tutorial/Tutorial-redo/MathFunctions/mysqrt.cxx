fbfe7d7e993ea444906921a842f3c6b249349a64
Your Name <you@example.com>
2021-08-26 18:07:58 -0700

    cmake tutorial s6. adding custom command and generated file

--- build-tools/cmake-tutorial/Tutorial-redo/MathFunctions/mysqrt.cxx
+++ build-tools/cmake-tutorial/Tutorial-redo/MathFunctions/mysqrt.cxx
@@ -1,19 +1,33 @@
 #include <iostream>
 #include <math.h>
 //#include <cmath.h>
+#include <Table.h>
 
 using namespace std;
 
-double mysqrt(double x) { 
-    
-    #if defined(HAVE_LOG) && defined(HAVE_EXP)
-      double result = exp(log(x) * 0.5);
-      std::cout << "Computing sqrt of " << x << " to be " << result
-                << " using log and exp" << std::endl;
-    #else
-      double result = x;
-    #endif
+double mysqrt(double x)
+{
+  if (x <= 0) {
+    return 0;
+  }
 
-    return result;
-        
+  // use the table to help find an initial value
+
+  double result = x;
+  if (x >= 1 && x < 10) {
+    std::cout << "Use the table to help find an initial value " << std::endl;
+    result = sqrtTable[static_cast<int>(x)];
+  }
+
+  // do ten iterations
+  for (int i = 0; i < 10; ++i) {
+    if (result <= 0) {
+      result = 0.1;
+    }
+    double delta = x - (result * result);
+    result = result + 0.5 * delta / result;
+    std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
+  }
+
+  return result;
 }
