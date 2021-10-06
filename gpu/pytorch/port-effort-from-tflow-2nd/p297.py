8552cffa29509f231d886e5423b4fa4ea4fabb19
Your Name <you@example.com>
2021-10-05 20:24:08 -0700

    p297.py: improvement, issue is data shape misalignment at loss computation

--- gpu/pytorch/port-effort-from-tflow-2nd/p297.py
+++ gpu/pytorch/port-effort-from-tflow-2nd/p297.py
@@ -22,8 +22,14 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor()
 )
 
+trainloader = torch.utils.data.DataLoader(training_data, batch_size=32, shuffle = True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle = True)
+
 print("training_data/test_data: ", type(training_data), len(training_data), type(test_data), len(test_data))
 print("type: ", type(training_data[0]))
+
+print("trainloader: ", type(trainloader))
+print("testloader:  ", type(testloader))
 '''
 # Defining input size, hidden layer size, output size and batch size respectively
 n_in, n_h, n_out, batch_size = 10, 5, 1, 10
@@ -36,34 +42,45 @@ n_in, n_h, n_out, batch_size = 10, 5, 1, 10
 # Create a model
 '''
 
+f1=nn.Flatten()
 l1=nn.Linear(28*28, 300)
 r1=nn.ReLU()
 l2=nn.Linear(300, 100)
 r2=nn.ReLU()
-l3=nn.Linear(100, 30)
-'''
-flatten = nn.Flatten()
-'''
-
+l3=nn.Linear(100, 10)
+s3=nn.Softmax()
 model = nn.Sequential(\
+    f1,
     l1,
     r1,
     l2,
     r2,  
-    l3)
+    l3, 
+    s3, \
+)
 
 print("Model: ", model)
+
+#print("model: layer0: ", model[0], model[0].weight)
+print("model: layer0: ", model[0])
 
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
 
 for epoch in range(50):
-    for (x, y) in enumerate(training_data):
+    for batch in trainloader:
+        print("batch: ", type(batch), ", ", len(batch))
+        imgs, lbls = batch
+        print("imgs: ", type(imgs), ", ", len(imgs), imgs.shape)
+        print("lbls: ", type(lbls), ", ", len(lbls), lbls.shape)
         # Forward pass: Compute predicted y by passing x to the model
-        y_pred = model(x)
+        y_pred = model(imgs)
+
+        print("y_pred: ", type(y_pred), y_pred.shape)
+        print("lbls:   ", type(lbls), lbls.shape)
 
         # Compute and print loss
-        loss = criterion(y_pred, y)
+        loss = criterion(y_pred, lbls)
         print('epoch: ', epoch,' loss: ', loss.item())
 
         # Zero gradients, perform a backward pass, and update the weights.
