38a3736ee1b616c2a87a3ff4a29b59973f51a4df
Your Name <you@example.com>
2021-10-23 01:13:34 +0800

    llvm: build.sh: output LLVM IR file: *.ll, comments added

--- llvm/build.sh
+++ llvm/build.sh
@@ -1,8 +1,25 @@
+# generate bitcode with -emit-llvm flag.
+
 clang -O3 -emit-llvm hello.c -c
-llvm-dis < hello.bc
+
+# input: bitcode, output: llvm ir.
+
+llvm-dis < hello.bc > hello.ll
+
+# jit input: bitcode.
+
 lli hello.bc
+
+# output assembly: input: bitcode, output: assembly.
+
 llc hello.bc -o hello.s
+
+# compile from assembly.
+
 clang hello.s -o hello.native 
+
+# compile from source.
+
 clang hello.c -o hello.direct
 echo hello.native md5sum: 
 md5sum hello.native
--- llvm/clean.sh
+++ llvm/clean.sh
@@ -2,4 +2,5 @@ rm hello.direct
 rm hello.native
 rm hello.s
 rm hello.bc
+rm *.ll
 rm *.out
