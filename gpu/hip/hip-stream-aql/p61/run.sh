f4185391dbf8d5c9190fc205a2a285cbf39a9070
Your Name <you@example.com>
2021-11-11 23:01:35 -0800

    p61-2streams-event.cpp

--- gpu/hip/hip-stream-aql/p61/run.sh
+++ gpu/hip/hip-stream-aql/p61/run.sh
@@ -2,7 +2,8 @@
 FILE1=p61
 FILE2=p61-2kernels
 FILE3=p61-2streams
-for FILE in $FILE3 ; do
+FILE4=p61-2streams-event
+for FILE in $FILE4 ; do
     echo "Processing for $FILE.cpp"
     hipcc $FILE.cpp -o $FILE.out
     #rocprof --hip-trace --hsa-trace --roctx-trace -i input.xml -d ./prof ./$FILE.out 
